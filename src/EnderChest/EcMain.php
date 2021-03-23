43445360ebdc7dd346f85dcf403d4057ead7a336
Fan-code04 <74674330+Fan-code04@users.noreply.github.com>
2021-03-23 03:38:13 +0100

    Update EcMain.php

--- src/EnderChest/EcMain.php
+++ src/EnderChest/EcMain.php
@@ -2,13 +2,18 @@
 
 namespace EnderChest;
 
-use pocketmine\inventory\EnderChestInventory;
+use pocketmine\block\Block;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\utils\Config;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\tile\EnderChest;
+use pocketmine\tile\Tile;
 use pocketmine\command\CommandSender;
 
 class EcMain extends PluginBase implements Listener{
@@ -26,7 +31,17 @@ class EcMain extends PluginBase implements Listener{
             case "ec":
                 if ($sender instanceof Player){
                     if ($sender->hasPermission("enderchest.cmd")){
-                     $ec = new EnderChestInventory();
+                     $nbt = new CompoundTag("", [new StringTag("id", Tile::CHEST), new StringTag("CustomName", "EnderChest"), new IntTag("x", (int)floor($sender->x)), new IntTag("y", (int)floor($sender->y) - 4), new IntTag("z", (int)floor($sender->z))]);
+                      
+			               $tile = Tile::createTile("EnderChest", $sender->getLevel(), $nbt);
+			               $block = Block::get(Block::ENDER_CHEST);
+			               $block->x = (int)$tile->x;
+			               $block->y = (int)$tile->y;
+			               $block->z = (int)$tile->z;
+			               $block->level = $tile->getLevel();
+			               $block->level->sendBlocks([$sender], [$block]);
+			               $sender->getEnderChestInventory()->setHolderPosition($tile);
+			               $sender->addWindow($sender->getEnderChestInventory());
                     } else{
                         $sender->sendMessage($this->cfg->get("no-perms"));
                     }
