508682ff6bcad976e5d79c37b5fb51d62daae992
Fan-code04 <74674330+Fan-code04@users.noreply.github.com>
2021-03-23 01:07:43 +0100

    Update EcMain.php

--- src/EnderChest/EcMain.php
+++ src/EnderChest/EcMain.php
@@ -9,4 +9,10 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 
 class EcMain extends PluginBase implements Listener{
+  
+  public function onEnable(){
+   $this->getLogger()->notice("EndesChest loaded ! ");
+  }
+  
+  
 }
