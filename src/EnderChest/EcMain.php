7f14abca689893bdad3624df648da6cc2f8c4f6a
Fan-code04 <74674330+Fan-code04@users.noreply.github.com>
2021-03-23 01:06:13 +0100

    Update EcMain.php

--- src/EnderChest/EcMain.php
+++ src/EnderChest/EcMain.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace EnderChest;
+
+use pocketmine\inventory\EnderChestInventory;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class EcMain extends PluginBase implements Listener{
+}
