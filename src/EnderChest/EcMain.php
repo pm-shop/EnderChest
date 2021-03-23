e18ed702b04dd8af8beefa8de3f7a2d47c17eff4
Fan-code04 <74674330+Fan-code04@users.noreply.github.com>
2021-03-23 01:13:21 +0100

    Update EcMain.php

--- src/EnderChest/EcMain.php
+++ src/EnderChest/EcMain.php
@@ -7,12 +7,34 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\utils\Config;
+use pocketmine\command\CommandSender;
 
 class EcMain extends PluginBase implements Listener{
   
   public function onEnable(){
-   $this->getLogger()->notice("EndesChest loaded ! ");
+   $this->getLogger()->notice("EnderChest loaded ! ");
+    
+    @mkdir($this->getDataFolder());
+        $this->saveResource("config.yml");
+        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
   }
   
-  
+  public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        switch ($command->getName()){
+            case "ec":
+                if ($sender instanceof Player){
+                    if ($sender->hasPermission("enderchest.cmd")){
+                     $ec = new EnderChestInventory();
+                    } else{
+                        $sender->sendMessage($this->cfg->get("no-perms"));
+                    }
+                } else{
+                    $sender->sendMessage($this->cfg->get("no-console"));
+                }
+                break;
+        }
+        return true;
+    }
 }
